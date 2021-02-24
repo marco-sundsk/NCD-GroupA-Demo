48ca684f79e79f6338ff09390669d5845eacfa22
qiao <qxwxintu@gmail.com>
2021-02-25 01:03:09 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,87 @@
 <template>
   <div id="root">
-    <SignedOut v-show="!isSignedIn" />
-    <SignedIn v-show="isSignedIn" />
+    <div class="bg-light">
+      <b-navbar class="container">
+        <b-navbar-brand href="#">NEAR DICE DAPP</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">How To Play</b-nav-item>
+            <b-nav-item href="#" disabled>Rules</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown text="Language" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">中文</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <!-- <b-nav-item-dropdown right>
+              <template #button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown> -->
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <div class="container pt-4">
+      <SignedOut v-show="!isSignedIn" />
+      <SignedIn v-show="isSignedIn" />
+
+      <div>
+        <b-card no-body>
+          <b-tabs card>
+            <b-tab title="ALL" active>
+              <div>
+                <b-card
+                  title="Card Title"
+                  class="mb-2"
+                >
+                  <b-card-text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </b-card-text>
+                </b-card>
+              </div>
+            </b-tab>
+            <b-tab title="WIN">
+              <b-card-text>Tab contents 2</b-card-text>
+            </b-tab>
+          </b-tabs>
+        </b-card>
+      </div>
+    </div>
+
+    <footer
+      class="bd-footer p-3 p-md-5 mt-5 bg-light text-center text-sm-start"
+    >
+      <div class="container text-center">
+        <p><strong> NEAR DICE DAPP</strong></p>
+        <span>Copyright 2021</span>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import "./global.css"
-import getConfig from "./config"
-import SignedOut from "./components/SignedOut.vue"
-import SignedIn from "./components/SignedIn.vue"
+import "./global.css";
+import getConfig from "./config";
+import SignedOut from "./components/SignedOut.vue";
+import SignedIn from "./components/SignedIn.vue";
 
-const nearConfig = getConfig(process.env.NODE_ENV || "development")
+const nearConfig = getConfig(process.env.NODE_ENV || "development");
 console.log(
   `networkId:${nearConfig.networkId} CONTRACT_NAME:${nearConfig.contractName}`
-)
-window.networkId = nearConfig.networkId
+);
+window.networkId = nearConfig.networkId;
 
 export default {
   created() {
-    document.title = "NCD-GroupA-Demo"
+    document.title = "NEAR DICE DAPP";
   },
   name: "App",
   components: {
@@ -29,9 +91,9 @@ export default {
 
   computed: {
     isSignedIn() {
-      return window.walletConnection.isSignedIn()
+      return window.walletConnection.isSignedIn();
     },
   },
-}
+};
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import Vue from "vue"
+import { BootstrapVue } from 'bootstrap-vue'
 import App from "./App.vue"
+
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+
+
 
 import { initContract } from "./utils"
 
@@ -7,10 +18,8 @@ Vue.config.productionTip = false
 
 window.nearInitPromise = initContract()
   .then(() => {
-
     new Vue({
       render: h => h(App),
     }).$mount("#app")
-
   })
   
