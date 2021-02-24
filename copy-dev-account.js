3c76a507ff3715781e449ee1ee6fe7a7732d8b02
marco <sun.dsk1@gmail.com>
2021-02-24 23:26:22 +0800

    fix yarn dev bug

--- copy-dev-account.js
+++ copy-dev-account.js
@@ -1,7 +1,9 @@
 let fs=require('fs')
 // copy created contract name for vue-cli-service at ./.env
-const contractNameFilePath = './neardev/dev-account';
-const existingContractName = fs.readFileSync(contractNameFilePath, { encoding: "utf8" }).trim();
+// const contractNameFilePath = './neardev/dev-account';
+// const existingContractName = fs.readFileSync(contractNameFilePath, { encoding: "utf8" }).trim();
+// dev-1614170766884-7229574
+const existingContractName = 'dev-1614170766884-7229574';
 const vueEnvFile = "./.env.development.local"
 fs.writeFileSync(vueEnvFile , "VUE_APP_CONTRACT_NAME=" + existingContractName)
-console.log(`copied ${contractNameFilePath} to ${vueEnvFile}`)
+// console.log(`copied ${contractNameFilePath} to ${vueEnvFile}`)
