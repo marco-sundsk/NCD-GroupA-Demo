dd31783ab80b8a502756572f239482670bd9aa64
qiao <qxwxintu@gmail.com>
2021-02-26 00:50:52 +0800

    add dice style

---
+++ src/components/Dice.vue
@@ -0,0 +1,289 @@
+<template>
+  <div id="dice" data-side="1">
+    <div class="sides side-1">
+      <span class="dot dot-1"></span>
+    </div>
+    <div class="sides side-2">
+      <span class="dot dot-1"></span>
+      <span class="dot dot-2"></span>
+    </div>
+    <div class="sides side-3">
+      <span class="dot dot-1"></span>
+      <span class="dot dot-2"></span>
+      <span class="dot dot-3"></span>
+    </div>
+    <div class="sides side-4">
+      <span class="dot dot-1"></span>
+      <span class="dot dot-2"></span>
+      <span class="dot dot-3"></span>
+      <span class="dot dot-4"></span>
+    </div>
+    <div class="sides side-5">
+      <span class="dot dot-1"></span>
+      <span class="dot dot-2"></span>
+      <span class="dot dot-3"></span>
+      <span class="dot dot-4"></span>
+      <span class="dot dot-5"></span>
+    </div>
+    <div class="sides side-6">
+      <span class="dot dot-1"></span>
+      <span class="dot dot-2"></span>
+      <span class="dot dot-3"></span>
+      <span class="dot dot-4"></span>
+      <span class="dot dot-5"></span>
+      <span class="dot dot-6"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dice",
+  props: {
+    start: {
+      type: Boolean,
+      default: () => false,
+    },
+    num: {
+      type: Number,
+      default: () => 0,
+    },
+  },
+  methods: {
+    rollDice: function () {
+      if (!(this.num >= 1 && this.num <= 6)) {
+        return;
+      }
+      let result = this.num;
+      let dice = document.getElementById("dice");
+      if (!dice) {
+        return;
+      }
+
+      dice.dataset.side = result;
+      dice.classList.toggle("reRoll");
+    },
+  },
+  computed: {},
+  watch: {
+    start: {
+      immediate: true,
+      handler() {
+        this.rollDice();
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+#dice {
+  width: 90px;
+  height: 90px;
+  transform-style: preserve-3d;
+  transition: transform 1.5s ease-out;
+}
+
+#dice:hover {
+  cursor: pointer;
+}
+
+.sides {
+  background-color: #efe5dc;
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.25);
+}
+
+.dot {
+  display: block;
+  position: absolute;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  background-color: black;
+  transform: translate(-50%, -50%);
+}
+
+.side-1 .dot-1 {
+  top: 50%;
+  left: 50%;
+}
+
+.side-2 .dot-1 {
+  top: 25%;
+  left: 25%;
+}
+.side-2 .dot-2 {
+  top: 75%;
+  left: 75%;
+}
+
+.side-3 .dot-1 {
+  top: 25%;
+  left: 25%;
+}
+.side-3 .dot-2 {
+  top: 75%;
+  left: 75%;
+}
+.side-3 .dot-3 {
+  top: 50%;
+  left: 50%;
+}
+
+.side-4 .dot-1 {
+  top: 25%;
+  left: 25%;
+}
+.side-4 .dot-2 {
+  top: 25%;
+  left: 75%;
+}
+.side-4 .dot-3 {
+  top: 75%;
+  left: 25%;
+}
+.side-4 .dot-4 {
+  top: 75%;
+  left: 75%;
+}
+
+.side-5 .dot-1 {
+  top: 25%;
+  left: 25%;
+}
+.side-5 .dot-2 {
+  top: 25%;
+  left: 75%;
+}
+.side-5 .dot-3 {
+  top: 75%;
+  left: 25%;
+}
+.side-5 .dot-4 {
+  top: 75%;
+  left: 75%;
+}
+.side-5 .dot-5 {
+  top: 50%;
+  left: 50%;
+}
+
+.side-6 .dot-1 {
+  top: 25%;
+  left: 25%;
+}
+.side-6 .dot-2 {
+  top: 25%;
+  left: 75%;
+}
+.side-6 .dot-3 {
+  top: 75%;
+  left: 25%;
+}
+.side-6 .dot-4 {
+  top: 75%;
+  left: 75%;
+}
+.side-6 .dot-5 {
+  top: 50%;
+  left: 25%;
+}
+.side-6 .dot-6 {
+  top: 50%;
+  left: 75%;
+}
+
+#dice .side-1 {
+  transform: translateZ(45px);
+}
+#dice .side-2 {
+  transform: rotateX(-180deg) translateZ(45px);
+}
+#dice .side-3 {
+  transform: rotateY(90deg) translateZ(45px);
+}
+#dice .side-4 {
+  transform: rotateY(-90deg) translateZ(45px);
+}
+#dice .side-5 {
+  transform: rotateX(90deg) translateZ(45px);
+}
+#dice .side-6 {
+  transform: rotateX(-90deg) translateZ(45px);
+}
+
+#dice[data-side="1"] {
+  transform: rotateX(360deg) rotateY(360deg) rotateZ(720deg);
+}
+#dice[data-side="2"] {
+  transform: rotateX(360deg) rotateY(540deg) rotateZ(720deg);
+}
+#dice[data-side="3"] {
+  transform: rotateX(360deg) rotateY(630deg) rotateZ(720deg);
+}
+#dice[data-side="4"] {
+  transform: rotateX(360deg) rotateY(450deg) rotateZ(720deg);
+}
+#dice[data-side="5"] {
+  transform: rotateX(630deg) rotateY(360deg) rotateZ(720deg);
+}
+#dice[data-side="6"] {
+  transform: rotateX(450deg) rotateY(360deg) rotateZ(720deg);
+}
+
+#dice[data-side="1"].reRoll {
+  transform: rotateX(0deg) rotateY(0deg) rotateZ(0deg);
+}
+#dice[data-side="2"].reRoll {
+  transform: rotateX(0deg) rotateY(180deg) rotateZ(0deg);
+}
+#dice[data-side="3"].reRoll {
+  transform: rotateX(0deg) rotateY(270deg) rotateZ(0deg);
+}
+#dice[data-side="4"].reRoll {
+  transform: rotateX(0deg) rotateY(90deg) rotateZ(0deg);
+}
+#dice[data-side="5"].reRoll {
+  transform: rotateX(270deg) rotateY(0deg) rotateZ(0deg);
+}
+#dice[data-side="6"].reRoll {
+  transform: rotateX(90deg) rotateY(0deg) rotateZ(0deg);
+}
+
+#diceResult {
+  position: absolute;
+  bottom: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: #ee4e34;
+  color: white;
+  font-weight: bold;
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.25);
+}
+
+#diceResult.hide {
+  opacity: 0;
+}
+
+#diceResult.reveal {
+  animation: fadeUp 0.3s forwards;
+}
+
+@keyframes fadeUp {
+  0% {
+    opacity: 0;
+    bottom: 0;
+  }
+
+  100% {
+    opacity: 1;
+    bottom: 20px;
+  }
+}
+</style>
--- src/components/SignedIn.vue
+++ src/components/SignedIn.vue
@@ -1,19 +1,29 @@
 <template>
   <div>
-    <button class="link" style="float: right" v-on:click="logout">Sign out</button>
+    <button class="link" style="float: right" v-on:click="logout">
+      Sign out
+    </button>
     <main>
       <h1>
-        Welcome {{ accountId }}, You have {{this.leftCount}} time to dice
+        Welcome {{ accountId }}, You have {{ this.leftCount }} time to dice
       </h1>
       <form v-on:submit.prevent="buyDice">
         <fieldset ref="fieldset">
           <label
             for="buyDice"
-            style="display:block; color:var(--gray);margin-bottom:0.5em;"
-          >Buy Dice</label>
-          <div style="display:flex">
-            Buy<input v-model="rollCount" autocomplete="off" id="roll" style="flex:1" />times
-            <button id="buy_dice" style="border-radius:0 5px 5px 0">Buy</button>
+            style="display: block; color: var(--gray); margin-bottom: 0.5em"
+          >Buy Dice</label
+          >
+          <div style="display: flex">
+            Buy<input
+              v-model="rollCount"
+              autocomplete="off"
+              id="roll"
+              style="flex: 1"
+            />times
+            <button id="buy_dice" style="border-radius: 0 5px 5px 0">
+              Buy
+            </button>
           </div>
         </fieldset>
       </form>
@@ -22,12 +32,21 @@
         <fieldset ref="fieldset">
           <label
             for="rollDice"
-            style="display:block; color:var(--gray);margin-bottom:0.5em;"
-          >Roll Dice</label>
-          <div style="display:flex">
-            <input v-model="rollNumber" autocomplete="off" id="roll" style="flex:1" />
-            <button id="roll_dice" style="border-radius:0 5px 5px 0">Roll</button>
+            style="display: block; color: var(--gray); margin-bottom: 0.5em"
+          >Roll Dice</label
+          >
+          <div style="display: flex">
+            <input
+              v-model="rollNumber"
+              autocomplete="off"
+              id="roll"
+              style="flex: 1"
+            />
+            <button id="roll_dice" style="border-radius: 0 5px 5px 0">
+              Roll
+            </button>            
           </div>
+          <Dice :start="diceStart" :num="diceNum" v-show="diceShow" />
         </fieldset>
       </form>
     </main>
@@ -44,28 +63,32 @@
 </template>
 
 <script>
-import { logout } from "../utils"
+import { logout } from "../utils";
 
-import Notification from "./Notification.vue"
+import Notification from "./Notification.vue";
+import Dice from "./Dice.vue";
 
 export default {
   name: "SignedIn",
 
   components: {
     Notification,
+    Dice,
   },
-
   data: function () {
     return {
-      gas:Math.pow(10,14).toString(),
-      at:"000000000000000000000000",
+      gas: Math.pow(10, 14).toString(),
+      at: "000000000000000000000000",
       savedGreeting: "",
       newGreeting: "",
-      leftCount:0,
-      rollCount:"",
-      rollNumber:"",
+      leftCount: 0,
+      rollCount: "",
+      rollNumber: "",
       notificationVisible: false,
-    }
+      diceShow: false,
+      diceStart: false,
+      diceNum:0,
+    };
   },
 
   created() {
@@ -74,16 +97,18 @@ export default {
 
   computed: {
     isSignedIn() {
-      return window.walletConnection? window.walletConnection.isSignedIn(): false
+      return window.walletConnection
+        ? window.walletConnection.isSignedIn()
+        : false;
     },
     accountId() {
-      return window.accountId
+      return window.accountId;
     },
     contractId() {
-      return window.contract? window.contract.contractId: null
+      return window.contract ? window.contract.contractId : null;
     },
     networkId() {
-      return window.networkId
+      return window.networkId;
     },
   },
 
@@ -93,85 +118,96 @@ export default {
       window.contract
         .get_account_dice_count({ account_id: window.accountId })
         .then((leftCount) => {
-          this.leftCount = leftCount
-        })
+          this.leftCount = leftCount;
+        });
+    },
+    showDice: function (num) {
+      this.diceNum=num
+      this.diceStart = !this.diceStart;
     },
 
     buyDice: async function () {
       // fired on form submit button used to update the greeting
 
       // disable the form while the value gets updated on-chain
-      this.$refs.fieldset.disabled = true
+      this.$refs.fieldset.disabled = true;
 
       try {
-        
         // make an update call to the smart contract
         await window.contract.buy_dice(
           {},
           this.gas,
-          parseInt(this.rollCount)+this.at
-        )
+          parseInt(this.rollCount) + this.at
+        );
       } catch (e) {
-        console.log(e)
+        console.log(e);
       } finally {
         // re-enable the form, whether the call succeeded or failed
-        this.$refs.fieldset.disabled = false
+        this.$refs.fieldset.disabled = false;
       }
 
-      this.notificationVisible = true //show new notification
+      this.notificationVisible = true; //show new notification
 
       // remove Notification again after css animation completes
       // this allows it to be shown again next time the form is submitted
       setTimeout(() => {
-        this.notificationVisible = false
-      }, 11000)
-
+        this.notificationVisible = false;
+      }, 11000);
     },
 
     rollDice: async function () {
+      this.diceShow=true
       // fired on form submit button used to update the greeting
 
       // disable the form while the value gets updated on-chain
-      this.$refs.fieldset.disabled = true
+      this.$refs.fieldset.disabled = true;
 
       try {
-        
         // make an update call to the smart contract
-        await window.contract.roll_dice(
-          {
+        await window.contract
+          .roll_dice({
             target: parseInt(this.rollNumber),
-          }
-        ).then((res)=>{
-          if (res.dice_point===res.user_guess) {
-            const reward_amount = res.reward_amount.toString()
-            const temp_amount = reward_amount.substr(0,reward_amount.length-20)
-            const int_part = temp_amount.substr(0,temp_amount.length-4)
-            const float_part = temp_amount.substr(0,int_part.legnth)
-            alert('Congratulations to you, you win '+int_part+'.'+float_part+' near')
-          } else {
-            alert('You lose, the number is '+res.dice_point)
-          }
-          this.leftCount = this.leftCount-1
-          console.log(res)
-        });
+          })
+          .then((res) => {
+            this.showDice(res.dice_point)
+            if (res.dice_point === res.user_guess) {
+              const reward_amount = res.reward_amount.toString();
+              const temp_amount = reward_amount.substr(
+                0,
+                reward_amount.length - 20
+              );
+              const int_part = temp_amount.substr(0, temp_amount.length - 4);
+              const float_part = temp_amount.substr(0, int_part.legnth);
+              alert(
+                "Congratulations to you, you win " +
+                    int_part +
+                    "." +
+                    float_part +
+                    " near"
+              );
+            } else {
+              alert("You lose, the number is " + res.dice_point);
+            }
+            this.leftCount = this.leftCount - 1;
+            console.log(res);
+          });
       } catch (e) {
-        console.log(e) //re-throw
+        console.log(e); //re-throw
       } finally {
         // re-enable the form, whether the call succeeded or failed
-        this.$refs.fieldset.disabled = false
+        this.$refs.fieldset.disabled = false;
       }
 
-      this.notificationVisible = true //show new notification
+      this.notificationVisible = true; //show new notification
 
       // remove Notification again after css animation completes
       // this allows it to be shown again next time the form is submitted
       setTimeout(() => {
-        this.notificationVisible = false
-      }, 11000)
-
+        this.notificationVisible = false;
+      }, 11000);
     },
 
     logout: logout,
   },
-}
+};
 </script>
